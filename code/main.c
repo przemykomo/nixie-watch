afd808d23192d7b14c084689af53dec3be4d803f
Przemyk <przemek@przemyk.xyz>
2023-04-19 12:14:04 +0200

    allow user to set time

--- code/main.c
+++ code/main.c
@@ -6,53 +6,69 @@
 #define SHIFT_DATA (1 << PB3)
 
 #define CLOCK_DELAY 20
+#define CLOCK_DELAY_FAST 1
 
 #define SHIFT_DIRECTION_PORT DDRB
 #define SHIFT_PORT PORTB
 
+#define BUTTON (1 << PB1)
+#define BUTTON_DIRECTION_PORT DDRB
+#define BUTTON_PORT PINB
+
 #define MAX_DIGIT 11
-
-
-#define RTC_ADDR 0b1101000
-const uint8_t addr = 0b1101000;
-
-void rtc_init() {
-    i2c_master_init();
-    /*
-    i2c_master_start_condition();
-    i2c_master_write_byte(0xD0);
-    i2c_master_write_byte(0x07); // address of the control register
-    i2c_master_write_byte(0b00010001);
-    i2c_master_stop_condition();*/
-    uint8_t data = 0b00010000;
-    i2c_master_write(0x68, 0x07, &data, 1);
-    data = 0b00000001;
-    i2c_master_write(0x68, 0x00, &data, 1);
-    data = 0b00000001;
-    i2c_master_write(0x68, 0x01, &data, 1);
-    data = 0b00000001;
-    i2c_master_write(0x68, 0x02, &data, 1);
-    data = 0b00000001;
-    i2c_master_write(0x68, 0x03, &data, 1);
-    data = 0b00000001;
-    i2c_master_write(0x68, 0x04, &data, 1);
-    data = 0b00000001;
-    i2c_master_write(0x68, 0x05, &data, 1);
-    data = 0b00000001;
-    i2c_master_write(0x68, 0x06, &data, 1);
-}
-/*
-void rtc_set_time(uint8_t h, uint8_t m, uint8_t s) {
-    i2c_master_start_condition();
-    i2c_master_write_byte(0xD0);
-    i2c_master_write_byte(0x00);
-    i2c_master_write_byte(s);
-    i2c_master_write_byte(m);
-    i2c_master_write_byte(h);
-    i2c_master_stop_condition();
-}*/
+#define LHDP 10
+#define RHDP 11
 
 int last_second = MAX_DIGIT;
+
+// Since the argument to _delay_ms has to be known at compile time, I just duplicated the entire write function.
+void write_fast(int first, int second) {
+    SHIFT_PORT &= ~SHIFT_CLOCK; // set up low clock
+
+    // move the last digit enough so that it disappears when the final number is displayed
+    for (int i = 0; i < (MAX_DIGIT - last_second - first); i++) {
+        SHIFT_PORT |= SHIFT_CLOCK;
+        _delay_ms(CLOCK_DELAY_FAST);
+        SHIFT_PORT &= ~SHIFT_CLOCK;
+        _delay_ms(CLOCK_DELAY_FAST);
+    }
+
+    last_second = second;
+
+    SHIFT_PORT |= SHIFT_DATA; // set the bit on to shift
+    _delay_ms(CLOCK_DELAY_FAST);
+    SHIFT_PORT |= SHIFT_CLOCK; // data is shifted on the rising edge
+    _delay_ms(CLOCK_DELAY_FAST);
+    
+    SHIFT_PORT &= ~SHIFT_CLOCK; // data is saved on the falling edge
+    SHIFT_PORT &= ~SHIFT_DATA; // disable the bit to shift
+    _delay_ms(CLOCK_DELAY_FAST);
+
+    // move the first bit by the difference between first and second bit placement
+    for (int i = 0; i < (first + MAX_DIGIT - second); i++) {
+        SHIFT_PORT |= SHIFT_CLOCK;
+        _delay_ms(CLOCK_DELAY_FAST);
+        SHIFT_PORT &= ~SHIFT_CLOCK;
+        _delay_ms(CLOCK_DELAY_FAST);
+    }
+
+    SHIFT_PORT &= ~SHIFT_CLOCK; // set up low clock
+    SHIFT_PORT |= SHIFT_DATA; // set the bit on to shift
+    _delay_ms(CLOCK_DELAY_FAST);
+    SHIFT_PORT |= SHIFT_CLOCK; // data is shifted on the rising edge
+    _delay_ms(CLOCK_DELAY_FAST);
+    
+    SHIFT_PORT &= ~SHIFT_CLOCK; // data is saved on the falling edge
+    SHIFT_PORT &= ~SHIFT_DATA; // disable the bit to shift
+    _delay_ms(CLOCK_DELAY_FAST);
+
+    for (int i = 0; i < second; i++) {
+        SHIFT_PORT |= SHIFT_CLOCK;
+        _delay_ms(CLOCK_DELAY_FAST);
+        SHIFT_PORT &= ~SHIFT_CLOCK;
+        _delay_ms(CLOCK_DELAY_FAST);
+    }
+}
 
 void write(int first, int second) {
     SHIFT_PORT &= ~SHIFT_CLOCK; // set up low clock
@@ -102,54 +118,91 @@ void write(int first, int second) {
     }
 }
 
+bool last_button_state = false;
+
 int main() {
 
     // Set the SPI nixie driver pins to output
-    //SHIFT_DIRECTION_PORT |= SHIFT_CLOCK | SHIFT_DATA;
+    SHIFT_DIRECTION_PORT |= SHIFT_CLOCK | SHIFT_DATA;
+
+    BUTTON_DIRECTION_PORT &= ~BUTTON;
     
-    rtc_init();
-    DDRB |= (1 << PB4);
+    i2c_master_init();
 
-    while (1) {
-        _delay_ms(10000);
-        uint8_t data = 0;
+    // If the button is pressed while the circuit gets powered on, then let the user set the hour.
+    if (BUTTON_PORT & BUTTON) {
+        while (BUTTON_PORT & BUTTON);
+        _delay_ms(50);
+        uint8_t hours = 0;
+        uint8_t minutes = 0;
 
-        i2c_master_read(0x68, 0x01, &data, 1);
-        for (uint8_t i = 0; i < data; i++) {
-            _delay_ms(200);
-            PORTB |= (1 << PB4);
-            _delay_ms(200);
-            PORTB &= ~(1 << PB4);
+        write(hours % 10, hours / 10);
+        
+        for (int i = 0; i < 500; i++) {
+            _delay_ms(10);
+            if (BUTTON_PORT & BUTTON) {
+                while (BUTTON_PORT & BUTTON);
+                _delay_ms(50);
+                i = 0;
+                hours++;
+                hours %= 24;
+                write_fast(hours % 10, hours / 10);
+            }
         }
+
+        write(minutes % 10, minutes / 10);
+        
+        for (int i = 0; i < 500; i++) {
+            _delay_ms(10);
+            if (BUTTON_PORT & BUTTON) {
+                while (BUTTON_PORT & BUTTON);
+                _delay_ms(50);
+                i = 0;
+                minutes++;
+                minutes %= 60;
+                write_fast(minutes % 10, minutes / 10);
+            }
+        }
+
+        uint8_t data[] = {
+            0x00, // seconds
+            ((minutes / 10) << 4) | (minutes % 10),
+            ((hours / 10) << 4) | (hours % 10),
+            0x03, // day of the week
+            0x19, // day of the month
+            0x04, // month
+            0x23, // year
+            0b00000000
+        };
+        i2c_master_write(0x68, 0x00, data, 8);
     }
-    /*
-    rtc_set_time(0x00, 0x00, 0x00);
-    uint8_t seconds = 0;
-    uint8_t minutes = 0;
-    uint8_t hours = 0;
+
+    write(RHDP, LHDP);
 
     while (1) {
-        _delay_ms(4000);
-        i2c_master_start_condition();
-        i2c_master_write_byte(0xD0);
-        i2c_master_write_byte(0x00);
-        i2c_master_start_condition();
-        i2c_master_write_byte(0xD1);
-        i2c_master_read_byte(&seconds, 0);
-        i2c_master_read_byte(&minutes, 0);
-        i2c_master_read_byte(&hours, 1);
-        i2c_master_stop_condition();
+        _delay_ms(10);
 
-        //write(seconds & 0xF, seconds & 0xF);
-
-        for (uint8_t i = 0; i < (minutes & 0b00001111); i++) {
-            _delay_ms(200);
-            SHIFT_PORT |= SHIFT_CLOCK;
-            _delay_ms(200);
-            SHIFT_PORT &= ~SHIFT_CLOCK;
+        bool button_state = (BUTTON_PORT & BUTTON);
+        if (button_state != last_button_state && button_state) {
+            _delay_ms(50);
+            button_state = (BUTTON_PORT & BUTTON); // debouncing
+            if (button_state) {
+                // For some reason when I try to read multiple bytes at the same time I get invalid data.
+                // So I decided not to handle the 59 -> 0 minutes edge case.
+                uint8_t hours;
+                uint8_t minutes;
+                i2c_master_read(0x68, 0x02, &hours, 1);
+                i2c_master_read(0x68, 0x01, &minutes, 1);
+                write(hours & 0x0F, (hours & 0xF0) >> 4);
+                _delay_ms(1000);
+                write(minutes & 0x0F, (minutes & 0xF0) >> 4);
+                _delay_ms(1000);
+                write(RHDP, LHDP);
+            }
         }
-    }*/
-    while (1);
+
+        last_button_state = button_state;
+    } 
 
     return 0;
 }
